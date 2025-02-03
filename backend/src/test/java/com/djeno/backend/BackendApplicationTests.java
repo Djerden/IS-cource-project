9610893c5044c92430da34e7205f6341a6096b74
Djeno <djeno00@mail.ru>
2025-02-03 16:24:12 +0300

    project structure

---
+++ backend/src/test/java/com/djeno/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djeno.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
